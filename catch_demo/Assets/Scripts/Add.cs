3e9a2192de02eabee4a3a2ad5b9390c414fb4086
e1b19094 <e1b19094@oit.ac.jp>
2022-10-13 13:34:21 +0900

    2nd week of October

---
+++ catch_demo/Assets/Scripts/Add.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Add : MonoBehaviour
+{
+    public GameObject prefabObj;
+
+    int tmp = 0;
+    float cnt = 0.0f;
+
+    // 初期化
+    void Start()
+    {
+
+    }
+
+    public void OnClick()
+    {
+        string number;
+        tmp++;
+
+        number = tmp.ToString();
+
+        // プレハブを元にオブジェクトを生成する
+        GameObject obj = Instantiate(prefabObj, new Vector3(-22.5f, 5.0f, 0.0f + cnt), Quaternion.Euler( 0, 180, 0));
+
+        GameObject child = obj.transform.Find("Canvas").gameObject;
+
+        child.transform.Find("Text").GetComponent<Text>().text = "File" + number;
+
+        cnt = cnt + 0.5f;
+        if (cnt > 1.0f)
+        {
+            cnt = -1.0f;
+        }
+    }
+}
